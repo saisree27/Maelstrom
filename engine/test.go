46c1d3c8926c999e18eefcaa3f3fe71a852481d2
saisree27 <saigautambonam@gmail.com>
2022-07-07 11:58:52 -0400

    Make move testing loop

--- engine/test.go
+++ engine/test.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func Test() {
 	b := newBoard()
-	m := []string{
-		"h2h4", "a7a5", "h4h5", "g7g5", "h5g6", "a5a4", "b2b4", "a4b3", "g6g7", "b3b2", "g7h8q", "b2a1b"}
-	for i := 0; i < len(m); i++ {
-		n := fromUCI(m[i], b)
-		b.makeMove(n)
-		fmt.Println(n.toUCI())
-	}
 	b.print()
 	b.printFromBitBoards()
+	for {
+		var move string
+		fmt.Println("Enter move: ")
+		fmt.Scanln(&move)
+
+		b.makeMove(fromUCI(move, b))
+		b.print()
+		b.printFromBitBoards()
+	}
 }
