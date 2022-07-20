d195b701d7be50d98564ee2407bc04ac6a08286c
saisree27 <saigautambonam@gmail.com>
2022-07-19 23:24:21 -0400

    Remove perft test

--- engine/perft_test.go
+++ engine/perft_test.go
@@ -4,11 +4,11 @@ import "testing"
 
 func TestPerft(t *testing.T) {
 	// Current fastest: 22.031s
-	RunTests()
+	// RunTests()
 }
 
 func TestSearch(t *testing.T) {
 	// Current fastest: 36.926s
-	initializeTTable(4096)
-	RunSearch("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 5)
+	// initializeTTable(1024)
+	// RunSearch("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4)
 }
