86702ac6c03b7c63b1cee9306567570c1dbe1327
saisree27 <saigautambonam@gmail.com>
2025-07-01 22:05:52 -0500

    Add tuned parameters for search based on WF run
    Score of Maelstrom TEST vs Maelstrom v3.0.0: 496 - 359 - 494  [0.551] 1349
    ...      Maelstrom TEST playing White: 266 - 173 - 235  [0.569] 674
    ...      Maelstrom TEST playing Black: 230 - 186 - 259  [0.533] 675
    ...      White vs Black: 452 - 403 - 494  [0.518] 1349
    Elo difference: 35.4 +/- 14.8, LOS: 100.0 %, DrawRatio: 36.6 %
    SPRT: llr 2.91 (100.7%), lbound -2.25, ubound 2.89 - H1 was accepted
    
    Player: Maelstrom TEST
       "Draw by 3-fold repetition": 304
       "Draw by fifty moves rule": 90
       "Draw by insufficient mating material": 95
       "Draw by stalemate": 5
       "Loss: Black mates": 173
       "Loss: White mates": 186
       "No result": 7
       "Win: Black mates": 230
       "Win: White mates": 266
    Player: Maelstrom v3.0.0
       "Draw by 3-fold repetition": 304
       "Draw by fifty moves rule": 90
       "Draw by insufficient mating material": 95
       "Draw by stalemate": 5
       "Loss: Black mates": 230
       "Loss: White mates": 266
       "No result": 7
       "Win: Black mates": 173
       "Win: White mates": 186
    Finished match

--- engine/tunable.go
+++ engine/tunable.go
@@ -15,15 +15,15 @@ type SearchParams struct {
 }
 
 var Params = SearchParams{
-	RFP_MULT:            120,
+	RFP_MULT:            132,
 	RFP_MAX_DEPTH:       9,
-	RAZORING_MULT:       220,
+	RAZORING_MULT:       228,
 	RAZORING_MAX_DEPTH:  2,
-	FUTILITY_BASE:       50,
-	FUTILITY_MULT:       150,
+	FUTILITY_BASE:       51,
+	FUTILITY_MULT:       142,
 	FUTILITY_MAX_DEPTH:  8,
 	IIR_MIN_DEPTH:       4,
 	IIR_DEPTH_REDUCTION: 1,
-	LMR_MIN_DEPTH:       3,
-	NMP_MIN_DEPTH:       3,
+	LMR_MIN_DEPTH:       2,
+	NMP_MIN_DEPTH:       2,
 }
